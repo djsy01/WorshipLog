ae544eeb563a17831fded6a82c29337cb69355d8
djsy01 <dlsgh8884@naver.com>
2026-05-04 19:45:25 +0900

    refactor: 보안절차 확인

--- client/src/components/AppHeader.tsx
+++ client/src/components/AppHeader.tsx
@@ -56,7 +56,7 @@ export default function AppHeader({ page }: AppHeaderProps) {
         }
         localStorage.clear();
         setShowLogoutModal(false);
-        router.replace('/dashboard');
+        window.location.replace('/dashboard');
     };
 
     return (
